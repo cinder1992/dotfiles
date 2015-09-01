d7b43fab91e6425a4082d63766af5e039f3c3741
cinder1992 <cinder5832@gmail.com>
2015-09-01 14:34:37 +0100

    Removed surpufluous startup files

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -490,14 +490,4 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
     
 -- }}}\
--- autostart
-function run_once(cmd)
-  findme = cmd
-  firstspace = cmd:find(" ")
-  if firstspace then
-    findme = cmd:sub(0, firstspace-1)
-  end
-  awful.util.spawn_with_shell("pgrep -u $USER -x " .. findme .. " > /dev/null || (" .. cmd .. ")")
-end
 
-run_once("mpd")
