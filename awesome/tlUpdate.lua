ebf83fe2a25e472ae66b4895677418701385b1da
cinder1992 <cinder5832@gmail.com>
2016-01-26 19:09:06 +0000

    Fixed errata when updateTaglist was used with the tasklist object

--- awesome/tlUpdate.lua
+++ awesome/tlUpdate.lua
@@ -18,71 +18,98 @@ local beautiful = require("beautiful")
 function updateTaglist(w, buttons, label, data, objects)
   -- code borrowed from common.list_update, modified to add an extra text seperator
   w:reset()
+  --print("Ran update!")
   local theme = beautiful.get()
   for i, o in ipairs(objects) do
     local cache = data[o]
-    local ib, tb, bgb, bgbs, ls, m, l, mt, sb
+    local ib, tb, bgb, bgbs, bgbl, m, mt, ml, sb, sbl, l, ls, ll
     if cache then
       ib = cache.ib
       tb = cache.tb
       bgb = cache.bgb
       bgbs = cache.bgbs -- seperator background
+      bgbl = cache.bgbl
       m = cache.m
       mt = cache.mt -- extra margin
+      ml = cache.ml
       sb = cache.sb -- seperator
+      sbl = cache.sbl
     else
       ib = wibox.widget.imagebox()
       tb = wibox.widget.textbox()
       sb = wibox.widget.textbox() -- seperator
+      sbl = wibox.widget.textbox() -- left seperator for first tag
+      bgbl = wibox.widget.background() -- left seperator background
       bgb = wibox.widget.background()
       bgbs = wibox.widget.background()
       m = wibox.layout.margin(tb, 0, 0)
       mt = wibox.layout.margin(sb, 0, 0)
+      ml = wibox.layout.margin(sbl, 0, 0)
       l = wibox.layout.fixed.horizontal()
       ls = wibox.layout.fixed.horizontal()
+      ll = wibox.layout.fixed.horizontal()
 
       l:fill_space(true)
       l:add(ib)
       l:add(m)
       ls:fill_space(true)
       ls:add(mt)
+      ll:fill_space(true)
+      ll:add(ml)
 
       bgb:set_widget(l)
       bgbs:set_widget(ls)
+      bgbl:set_widget(ll)
 
       bgb:buttons(common.create_buttons(buttons, o))
       bgbs:buttons(common.create_buttons(buttons, o))
+      bgbl:buttons(common.create_buttons(buttons, o))
 
 
       data[o] = {
         ib = ib,
         tb = tb,
+        bgbl = bgbl,
         bgb = bgb,
         bgbs = bgbs,
         m = m,
         mt = mt,
+        ml = ml,
         sb = sb,
+        sbl = sbl
       }
     end
+
     local text, bg, bg_image, icon = label(o)
     local bgl
     if objects[i+1] ~= nil then
       _, bgl, _ = label(objects[i+1])
     end
-    if bg ~= nil then
+    if (bg ~= nil and bg ~= theme.bg_normal) then
       sb:set_markup("<span color='" .. bg .. "'>" .. theme.sep_foc .. "</span>")
       if bgl ~= nil then
         bgbs:set_bg(bgl)
       else
         bgbs:set_bg(theme.bg_normal)
       end
-    elseif bgl ~= nil then
+    elseif (bgl ~= nil and bgl ~= theme.bg_normal) then
       local bgr = bg or theme.bg_normal
       sb:set_markup("<span color='" .. bgr .. "'>" .. theme.sep_foc .. "</span>")
       bgbs:set_bg(bgl)
     else
       sb:set_markup("<span color='" .. theme.fg_normal .. "'>" .. theme.sep_bak .. "</span>")
       bgbs:set_bg(theme.bg_normal)
+    end
+
+    if i == 1 then
+      if (bg ~= nil and bg ~= theme.bg_normal) then
+        sbl:set_markup("<span color='" .. theme.bg_normal .. "'>" .. theme.sep_foc .. "</span>")
+        bgbl:set_bg(bg)
+      else
+        sbl:set_markup("<span color='" .. theme.fg_normal .. "'>" .. theme.sep_bak .. "</span>")
+        bgbl:set_bg(theme.bg_normal)
+      end
+      w:add(bgbl)
     end
 
     if not pcall(tb.set_markup, tb, text) then
