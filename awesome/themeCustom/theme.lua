f8cd4548e0bdf00c858a3c13a27bf6decd2e96c0
cinder1992 <cinder5832@gmail.com>
2016-01-26 16:12:52 +0000

    Restore tlUpdate.lua

--- awesome/themeCustom/theme.lua
+++ awesome/themeCustom/theme.lua
@@ -6,10 +6,10 @@ theme = {}
 
 theme.font          = "sans 8"
 
-theme.bg_normal     = "#222222"
-theme.bg_focus      = "#535d6c"
-theme.bg_urgent     = "#ff0000"
-theme.bg_minimize   = "#444444"
+theme.bg_normal     = "#03233B"
+theme.bg_focus      = "#2B7FBF"
+theme.bg_urgent     = "#DD9536"
+theme.bg_minimize   = "#293E4E"
 theme.bg_systray    = theme.bg_normal
 
 theme.fg_normal     = "#aaaaaa"
@@ -17,10 +17,10 @@ theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
-theme.border_width  = 0
-theme.border_normal = "#000000"
-theme.border_focus  = "#535d6c"
-theme.border_marked = "#91231c"
+theme.border_width  = 4
+theme.border_normal = "#03233B"
+theme.border_focus  = "#2B7FBF"
+theme.border_marked = "#DD9536"
 
 -- There are other variable sets
 -- overriding the default one when
