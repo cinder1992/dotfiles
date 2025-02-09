008419f15503e5b32881fdaf61482cdb1d208b8b
Neil Ray <cinder5832@gmail.com>
2025-02-09 15:34:17 +0000

    updated path

--- .zsh/config/10-path.zsh
+++ .zsh/config/10-path.zsh
@@ -1,4 +1,5 @@
 export GOPATH=~/go
-export PATH="$HOME/opt/gcc-5.3.0/bin:$GOPATH/bin:$PATH"
+export PATH="$HOME/opt/cross/bin:$GOPATH/bin:$PATH"
 export PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"
 export PATH="/home/neil/.rakudobrew/bin:$PATH"
+export PATH="/home/nray/.local/bin:$PATH"
